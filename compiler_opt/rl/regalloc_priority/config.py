a8b962936f8f05a53679f8ee87abb3843d69998c
Eric <flpha0830@gmail.com>
2022-07-15 17:54:37 -0400

    setup policy_network

--- compiler_opt/rl/regalloc_priority/config.py
+++ compiler_opt/rl/regalloc_priority/config.py
@@ -8,19 +8,18 @@ from compiler_opt.rl import feature_ops
 @gin.configurable()
 def get_regalloc_signature_spec():
     observation_spec = dict(
-        (key, tf.TensorSpec(dtype=tf.int64, shape=2, name=key))
-        for key in ('size', 'stage', 'mask'))
-    observation_spec['weight'] = tf.TensorSpec(dtype=tf.float32, shape=2, name='weight')
+        (key, tf.TensorSpec(dtype=tf.int64, shape=(), name=key))
+        for key in ('size', 'stage'))
+    observation_spec['weight'] = tf.TensorSpec(dtype=tf.float32, shape=(), name='weight')
 
     reward_spec = tf.TensorSpec(dtype=tf.float32, shape=(), name='reward')
     time_step_spec = time_step.time_step_spec(observation_spec, reward_spec)
 
-    action_spec = tensor_spec.BoundedTensorSpec(
-        dtype=tf.int64,
+    action_spec = tensor_spec.TensorSpec(
+        dtype=tf.float32,
         shape=(),
-        name='priority',
-        minimum=0,
-        maximum=1)
+        name='priority'
+    )
 
     return time_step_spec, action_spec
 
@@ -29,8 +28,6 @@ def get_regalloc_signature_spec():
 def get_observation_processing_layer_creator():
     def observation_processing_layer(obs_spec):
         """Creates the layer to process observation given obs_spec."""
-        if obs_spec.name == 'mask':
-            return tf.keras.layers.Lambda(feature_ops.discard_fn)
 
         if obs_spec.name in ('size', 'stage', 'weight'):
             return tf.keras.layers.Lambda(feature_ops.identity_fn)
